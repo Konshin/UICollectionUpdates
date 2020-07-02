cc49ec243b616448fa5a301e6910be91372cee33
Aleksei Konshin <alexey@konshin.net>
2020-07-02 17:08:23 +0300

    Added new public initializations for UICollectionSectionUpdates

--- UICollectionUpdates.podspec
+++ UICollectionUpdates.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'UICollectionUpdates'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'Wrapper for UITableView and UICollectionView updates with model-oriented behavior'
 
 # This description is used to generate tags and improve search results.
--- UICollectionUpdates/Classes/UICollectionSectionUpdates.swift
+++ UICollectionUpdates/Classes/UICollectionSectionUpdates.swift
@@ -9,14 +9,22 @@ import Foundation
 
 /// List of updates for one section
 public struct UICollectionSectionUpdates: Equatable {
-    public var reloadIndexes = IndexSet()
-    public var deleteIndexes = IndexSet()
-    public var insertIndexes = IndexSet()
+    public var reloadIndexes: IndexSet = []
+    public var deleteIndexes: IndexSet = []
+    public var insertIndexes: IndexSet = []
 
     public var isEmpty: Bool {
         return reloadIndexes.isEmpty
             && deleteIndexes.isEmpty
             && insertIndexes.isEmpty
+    }
+    
+    public init(reloadIndexes: IndexSet = [],
+                deleteIndexes: IndexSet = [],
+                insertIndexes: IndexSet = []) {
+        self.reloadIndexes = reloadIndexes
+        self.deleteIndexes = deleteIndexes
+        self.insertIndexes = insertIndexes
     }
 
 }
@@ -26,7 +34,7 @@ extension UICollectionSectionUpdates {
     /// Init from deletes/inserts diff
     ///
     /// - Parameter diff: Картеж дифа
-    init(diff: (deletes: [Int], inserts: [Int])) {
+    public init(diff: (deletes: [Int], inserts: [Int])) {
         self.insertIndexes = IndexSet(diff.inserts)
         self.deleteIndexes = IndexSet(diff.deletes)
         reloadIndexes = IndexSet()
